34eb0a645764eee98436df258b3d0ae196ca41b4
Skaroulis <nikodimos2003@gmail.com>
2024-06-26 15:29:16 +0300

    Final Light/Dark mode Toggle
    You aint ready

--- WebApp/src/main/webapp/JSScripts/ToggleViewMode.js
+++ WebApp/src/main/webapp/JSScripts/ToggleViewMode.js
@@ -1,5 +1,28 @@
+// Check local storage for theme on page load
+document.addEventListener('DOMContentLoaded', (event) => {
+    const body = document.body;
+    const icon = document.querySelector('.floating-btn .fas');
+    const theme = localStorage.getItem('theme');
 
+    if (theme === 'light') {
+        body.classList.add('light-theme');
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    }
+});
+// Toggles theme to the reverse of what it was and stores it locally
 function toggleTheme() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+    const body = document.body;
+    body.classList.toggle('light-theme');
+
+    const icon = document.querySelector('.floating-btn .fas');
+    if (body.classList.contains('light-theme')) {
+        localStorage.setItem('theme', 'light');
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        localStorage.setItem('theme', 'dark');
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
 }
